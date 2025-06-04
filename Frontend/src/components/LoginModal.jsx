7f30956230c270a4867ccbe77b9d59d98485b889
Sophia Pobre <sophiapobre@gmail.com>
2025-06-04 16:04:54 -0700

    Added routing to sign in & Google sign in buttons

--- Frontend/src/components/LoginModal.jsx
+++ Frontend/src/components/LoginModal.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -23,13 +24,15 @@ const style = {
 export default function LoginModal({ open, onClose }) {
   const [showSignIn, setShowSignIn] = useState(true);
 
+  const navigate = useNavigate();
+
   const handleClose = () => {
     setShowSignIn(true);
     onClose();
   }
 
   const handleSuccess = (response) => {
-    // placeholder function
+    navigate('/dashboard');
   };
 
   const handleError = (error) => {
@@ -57,6 +60,7 @@ export default function LoginModal({ open, onClose }) {
             sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 2 }}
             onSubmit={(event) => {
               event.preventDefault();
+              navigate('/dashboard');
             }}
           >
             {showSignIn ?  (null) : (
