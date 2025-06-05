b32fb3bdaa4d69313523f403d4df7dcfd27c262c
lavenderYuu <lavenderyu0113@gmail.com>
2025-06-04 22:26:54 -0700

    docker set up for frontend and backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Add this line
+    port: 5174,
+    strictPort: true,
+  },
   plugins: [react()],
 })
