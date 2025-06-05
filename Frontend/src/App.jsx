cd4dfd82201f4294d7a10d8c86f86a4f6334ff8a
lavenderYuu <lavenderyu0113@gmail.com>
2025-06-04 18:18:03 -0700

    Revert "Merge pull request #4 from CPSC455-2025S/lav-components"
    This reverts commit 86ad30b79a8317cbeff7ae3f03cdb2f1b1ed46c1, reversing
    changes made to db7a0050a0b100236518d8774d1d2d8d4552a8ff.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import NavigationBar from "./components/navigation.jsx";
 import Home from "./pages/Home"
 import Landing from "./pages/Landing"
 
@@ -8,11 +7,10 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   )
 }
