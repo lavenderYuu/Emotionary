fa39cd924d6a50ecb9c43d41674e9b97e828e5ed
Kathleen <ktom@students.cs.ubc.ca>
2025-06-19 22:03:02 -0700

    styling + add charts to insights page

--- Frontend/src/pages/Insights.jsx
+++ Frontend/src/pages/Insights.jsx
@@ -2,6 +2,8 @@ import MoodChart from "../components/MoodChart";
 import MoodCalendar from "../components/MoodCalendar";
 import MoodPieChart from "../components/MoodPieChart";
 import { Box, Grid } from "@mui/material";
+import MoodHeatmap from "../components/MoodHeatmap";
+import MoodGauge from "../components/MoodGauge";
 
 const Insights = () => {
   
@@ -9,14 +11,18 @@ const Insights = () => {
   return (
       <>
         <MoodChart />
-        <Grid container spacing={10} justifyContent="center">
-          <Grid item xs={12} md={8}>
-            <MoodCalendar />
+        <h2>Your Mood this Month</h2>
+        <Grid container spacing={20} justifyContent="center">
+          <Grid>
+            {/* <MoodCalendar /> */}
+            <MoodGauge />
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid>
             <MoodPieChart />
           </Grid>
         </Grid>
+        
+        <MoodHeatmap />
       </>
   )
 }
