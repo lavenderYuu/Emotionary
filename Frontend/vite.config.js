20cdbd9397c7bece2c899de07134b2a15ad1761b
aachung <aachung@student.ubc.ca>
2025-07-04 16:46:58 -0700

    update vite.config envDir path

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: true, // Add this line
+    host: true,
     port: 5174,
     strictPort: true,
   },
   plugins: [react()],
+  envDir: "../"
 })
